3d7bbed18998c9e71d27f596a9407b773ae943c0
ketsupL <neilmorales557@gmail.com>
2024-11-18 18:33:44 +0800

    Added login scene, signup scene, their fxml and css, and resources used

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,8 @@ module com.example.audiolibrary {
     requires com.dlsc.formsfx;
 
     opens com.example.audiolibrary to javafx.fxml;
+    opens com.example.audiolibrary.controllers to javafx.fxml;
+
     exports com.example.audiolibrary;
+    exports com.example.audiolibrary.controllers;
 }
