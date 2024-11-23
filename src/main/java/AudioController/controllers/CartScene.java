51a622f766ce101d7160d4e502db871b6fc8519b
ketsupL <neilmorales557@gmail.com>
2024-11-23 14:58:28 +0800

    my previous commit + jaymark's

--- src/main/java/AudioController/controllers/CartScene.java
+++ src/main/java/AudioController/controllers/CartScene.java
@@ -1,6 +1,37 @@
 package AudioController.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 
 public class CartScene {
 
+    @FXML
+    private Button checkoutButton;
+
+    private int orderCount = 0;
+
+    public void updateCheckoutButtonText() {
+        checkoutButton.setText("Check Out (" + orderCount + ")");
+    }
+
+    public void addOrder() {
+        orderCount++;
+        updateCheckoutButtonText();
+    }
+
+    public void clearOrders() {
+        orderCount = 0;
+        updateCheckoutButtonText();
+    }
+
+    @FXML
+    public void initialize() {
+        updateCheckoutButtonText();
+
+        // Create and set Tooltip for checkoutButton
+        Tooltip tooltip = new Tooltip("Click to complete your purchase");
+        checkoutButton.setTooltip(tooltip);
+
+    }
 }
