d13c192a056f419fa44052fd4cdcaa6f1269c30f
ketsupL <neilmorales557@gmail.com>
2024-11-23 14:56:24 +0800

    added button handling to ProfileScene.java, updated LibraryScene, mainpageScene, and profileScene fxmls.

--- src/main/java/AudioController/controllers/ProfileScene.java
+++ src/main/java/AudioController/controllers/ProfileScene.java
@@ -1,4 +1,52 @@
 package AudioController.controllers;
 
+import javafx.animation.Interpolator;
+import javafx.animation.ScaleTransition;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
+
 public class ProfileScene {
+
+    //Button UX
+    @FXML
+    private void handleButtonEntered(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(100), button);
+        scaleTransition.setToX(1.1);
+        scaleTransition.setToY(1.1);
+        scaleTransition.setInterpolator(Interpolator.EASE_BOTH);
+        scaleTransition.play();
+    }
+
+    @FXML
+    private void handleButtonExited(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(100), button);
+        scaleTransition.setToX(1.0);
+        scaleTransition.setToY(1.0);
+        scaleTransition.setInterpolator(Interpolator.EASE_OUT);
+        scaleTransition.play();
+    }
+
+    @FXML
+    private void handleButtonPressed(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(50), button);
+        scaleTransition.setToX(1.05);
+        scaleTransition.setToY(1.05);
+        scaleTransition.setInterpolator(Interpolator.EASE_BOTH);
+        scaleTransition.play();
+    }
+
+    @FXML
+    private void handleButtonReleased(MouseEvent event) {
+        Button button = (Button) event.getSource();
+        ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(50), button);
+        scaleTransition.setToX(1);
+        scaleTransition.setToY(1);
+        scaleTransition.setInterpolator(Interpolator.EASE_BOTH);
+        scaleTransition.play();
+    }
 }
